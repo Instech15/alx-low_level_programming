25a58f51052b7d4dfac9a4ccc991a8dd8ef4c6b9
Instech15 <adeniyiadewale15@gmail.com>
2022-09-08 16:19:53 -0700

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
-int main (Promul)
-{	/*Here, I initialize my variable, proMul.*/
-	char Promul[]="Programming is like building a multilingual puzzle";
-	/*Here, i'm using the put function to print the output*/
+# include <stdio.h>
+/**
+ *This program will use the puts function to print output
+ *Description: In the main function, I initialized my variable, Promul
+ *puts function is then used to print
+ *A 0 value is returned
+ */
+int main(void)
+{
+	char Promul[] = "Programming is like building a multilingual puzzle";
+
 	puts(Promul);
-	/*Lastly, I'm returning a value of zero. Unlike void, Integer must always return a value.*/
+
 	return (0);
 }
