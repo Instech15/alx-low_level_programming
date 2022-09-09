2b3479d6a8af030423f0730d184ba35e8c39a244
Instech15 <adeniyiadewale15@gmail.com>
2022-09-08 17:31:06 -0700

    Using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,9 +7,11 @@
  * Printf prints output
  * 0 Value is returned
 */
+
+/*main function*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	/*return value of 0*/
 	return (0);
 }
