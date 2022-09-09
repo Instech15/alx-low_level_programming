fea5e55cf83e05af78fc4eabf5bba46026367d76
Instech15 <adeniyiadewale15@gmail.com>
2022-09-08 17:55:11 -0700

    Using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 /**
- *Writes message with printf
- *
- * Description: This program uses printf function
+ * Description: main - Writes message with printf
+ * This program uses printf function
  * Main function
  * Printf prints output
- * 0 Value is returned
+ * Return: 0 Value is returned
 */
 
-/*main function*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/*return value of 0*/
 	return (0);
 }
