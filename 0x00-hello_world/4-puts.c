1bc9250c2aa5151054707cfff861f648f4b1ecfe
Instech15 <adeniyiadewale15@gmail.com>
2022-09-08 17:45:04 -0700

    First program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 # include <stdio.h>
 /**
- *Promul - This program will use the puts function to print output
- *@Promul: The variable
+ *Description: main - This program will use the puts function to print output
  *
- * Description: In the main function, I initialized my variable, Promul
+ *Variable is initialized
  *puts function is then used to print
- *A 0 value is returned
+ *Return: 0 value is returned
  */
 int main(void)
 {
+
 	char Promul[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Promul);
