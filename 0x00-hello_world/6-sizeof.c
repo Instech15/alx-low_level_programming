8e5ff472a75f12df5e0f8dc4e8e726e55b8e3226
Instech15 <adeniyiadewale15@gmail.com>
2022-09-08 19:20:31 -0700

    Size of types

--- 0x00-hello_world/6-sizeof.c
+++ 0x00-hello_world/6-sizeof.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-#include <curses.h>
 /**
  * Description: main - Printing size of various types
  *
  * Return: 0 is returned after a successfull run
  */
+
 int main(void)
 {
-	printf("int is %2ld bytes\n", sizeof(int));
-	printf("float is %ld bytes\n", sizeof(float));
-	printf("double if %ld bytes\n", sizeof(double));
-	printf("char is %ld bytes\n", sizeof(char));
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
 
 	return (0);
 }
