b1573739b562e2f96e4fc561d6a2f33c187027b6
Instech15 <adeniyiadewale15@gmail.com>
2022-09-13 11:00:52 -0700

    Printing lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase alphabets
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char wale_alpha = 'a';
+
+	while (wale_alpha <= 'z')
+	{
+		_putchar(wale_alpha);
+		_putchar('\n');
+
+		return (0);
+	}
+}
