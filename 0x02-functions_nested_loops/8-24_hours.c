0510948d246af5484680406ef5b068691fafd0c5
Instech15 <adeniyiadewale15@gmail.com>
2022-09-13 16:18:54 -0700

    24 Hours time print

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ */
+void jack_bauer(void)
+{
+	char i[5] = "00:00";
+
+	for (i = "00:00"; i <= "23:59"; i++)
+	_putchar('i');
+}
