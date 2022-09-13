c1698d5273e262c9696e9da4b4a7f1535a3a1c39
Instech15 <adeniyiadewale15@gmail.com>
2022-09-13 14:55:22 -0700

    Checks sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,11 +12,11 @@ int print_sign(int n)
 {
 	if (n > 0)
 		return (1);
-		_putchar('+');
+	_putchar('+');
 	if (n == 0)
 		return (0);
-		_putchar('0');
+	_putchar('0');
 	if (n < 0)
 		return (-1);
-		_putchar('-');
+	_putchar('-');
 }
