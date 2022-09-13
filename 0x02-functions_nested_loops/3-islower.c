e1f2b915cdd65691dc7f90e177eee0d832819a20
Instech15 <adeniyiadewale15@gmail.com>
2022-09-13 14:15:04 -0700

    Lowercase check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,15 @@
 
 /**
  * _islower - returns lowercase
- *
+ *@c: letter input to be checked
+
  * Return: 1 if c is lowercase
  * return 0 if otherwise
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-			return (1);
+		return (1);
 	else
-			return (0);
+		return (0);
 }
