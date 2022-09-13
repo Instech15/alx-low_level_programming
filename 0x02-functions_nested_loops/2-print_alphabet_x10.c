adbb9951a254470bdcff44af236902fd5aea8821
Instech15 <adeniyiadewale15@gmail.com>
2022-09-13 11:34:12 -0700

    Alphabets times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,17 @@
 void print_alphabet_x10(void)
 {
 	char wale_alpha = 'a';
+	int n = 0;
 
-	for (wale_alpha = 'a'; wale_alpha <= 'z'; wale_alpha + 10)
+	while (n < 10)
 	{
-		_putchar(wale_alpha);
-	}
+		for (wale_alpha = 'a'; wale_alpha <= 'z'; wale_alpha + 10)
+		{
+			_putchar(wale_alpha);
+		}
 
-	_putchar('\n');
+		n++;
+
+		_putchar('\n');
+	}
 }
