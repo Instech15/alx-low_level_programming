06fdf5b3bf1a7165cd80532dccfbb735054e9d16
Instech15 <adeniyiadewale15@gmail.com>
2022-09-13 17:59:34 -0700

    Printing natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,34 @@
  */
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
+	if (n < 98)
 	{
+		while (n <= 98)
+		{
+
 		printf("%d", n);
+		if (n != 98)
+		{
 		printf(", ");
+		}
+		n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+	printf("98");
 	}
 	printf("\n");
 }
