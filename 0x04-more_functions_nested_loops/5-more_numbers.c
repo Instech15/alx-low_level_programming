ede68c87173f8976af4298505aaa8a745975eef6
Instech15 <adeniyiadewale15@gmail.com>
2022-09-16 09:07:02 -0700

    Print 0 to 14 times 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,17 @@
  */
 void more_numbers(void)
 {
-	int n, o, p;
+	int n, o, p, q;
 
 	for (n = 0; n <= 9; n++)
 	{
 		for (o = 0; o <= 14; o++)
 		{
-			p = o * 10;
-			if (x != 0)
+			p = o / 10;
+			q = o % 10;
+			if (p != 0)
 				_putchar(p + '0');
+			_putchar(q + '0');
 		}
 		_putchar('\n');
 	}
