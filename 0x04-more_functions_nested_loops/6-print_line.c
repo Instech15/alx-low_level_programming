d16b36d5445a063b1bbb6ed8efa7631b9571fe4c
Instech15 <adeniyiadewale15@gmail.com>
2022-09-16 09:27:32 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - print straight line
+ *
+ * Return: Nothing to be returned
+ */
+void print_line(int n)
+{
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
