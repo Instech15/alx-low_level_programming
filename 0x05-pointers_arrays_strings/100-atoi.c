d40e5591b92157373dc818c8e7366c1f7b7ab888
Instech15 <adeniyiadewale15@gmail.com>
2022-09-19 15:15:33 -0700

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int _atoi(char *);
-
 /**
- * main - Converts strings to int
+ * _atoi - Converts strings to int
  *
  * Return: Always 0.
  */
-int main(void)
+int _atoi(char *)
 {
 	int n;
 
 	n = _atoi("4");
 	printf("%d\n", n);
+
 	return (0);
 }
