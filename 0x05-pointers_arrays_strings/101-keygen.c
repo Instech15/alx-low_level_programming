e7f5c196871d4000f135983cc92431248a6c0e21
Instech15 <adeniyiadewale15@gmail.com>
2022-09-19 14:43:51 -0700

    Generate random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Generates random password
+ *
+ *Return: Always 0
+*/
+int main(void)
+{
+	printf("Here is a random password: %d", rand());
+
+	return (0);
+}
+
