cfc05bdf5221f88d22d166ab17b575d6d8e9298f
Instech15 <adeniyiadewale15@gmail.com>
2022-09-19 14:23:22 -0700

    Converts number to string

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *_atoi - Converts string to integer
+ *@s: input string
+ *Return: Always 0 (Success)
+ */
+int _atoi(char *s)
+{
+
+		return (0);
+}
