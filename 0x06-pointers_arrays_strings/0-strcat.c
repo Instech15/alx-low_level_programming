7872f3d5a5de922efaeb2145ec3a694d298a67a4
Instech15 <adeniyiadewale15@gmail.com>
2022-09-21 10:13:13 -0700

    Concatenates two strings, using at most n bytes from src

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,8 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(src, dest);
-	_putchar(dest);
+	int i, n;
 
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
 	return (dest);
 }
