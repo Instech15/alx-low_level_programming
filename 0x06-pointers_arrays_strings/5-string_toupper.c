faf47c560be08b47900c0796a71dfdb9859efa84
Instech15 <adeniyiadewale15@gmail.com>
2022-09-21 12:12:29 -0700

    Changing lowercase string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * Author: Adewale Adeniyi
- * *_string_toupper - changes lowercase of a string to upper
+ * _string_toupper - changes lowercase of a string to upper
  * @n: the input string
  *
  * Return: uppercase string
