e44f9f3a7e2d1546181703b19c83376fa8c01358
Instech15 <adeniyiadewale15@gmail.com>
2022-09-21 14:53:56 -0700

    Encoding a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /*
- * rot13 - Entry point
+ * rot13 - encodes a string using rot 13
  * ONE if, TWO loops only...
- * @n: input
  * Return: decrypted string
  */
 
-char *rot13(char *n)
+char *rot13(char *)
 {
 	int x, rot_c = 13, i = 0;
 	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P', 'c',
