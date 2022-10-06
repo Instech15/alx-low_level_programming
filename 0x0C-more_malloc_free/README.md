d2bf2dc554e0b58a86e9e664e03d3285521fff62
Instech15 <adeniyiadewale15@gmail.com>
2022-10-06 13:42:37 -0700

    Malloc checked

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+More on using malloc and free
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+/**
+ * * _putchar - writes character c to stdout
+ * @c: The character to be printed
+ * *
+ * *Return: 1 is returned on success
+ * *On error, return -1.
+ * */
+int _putchar(char c)
+{
+	        return ((1, &c, 1));
+}
