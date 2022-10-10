e31120c0736eba9d756cc4b72a6e2de77e4d432b
Instech15 <adeniyiadewale15@gmail.com>
2022-10-10 11:06:30 -0700

    Defining structure in C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,8 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - struct definition
+ * struct dog - struct for char and float
  * @name: member1
  * @age: member2
  * @owner: member3
@@ -12,3 +15,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * Prototypes
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
